63920e9d12081b07d4bc7229f35c933aab45f3fc
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2022-01-27 19:39:11 -0300

    category - created findById method

--- backend/src/main/java/com/gabriel/catalog/services/CategoryService.java
+++ backend/src/main/java/com/gabriel/catalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.gabriel.catalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,14 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public List<CategoryDto> findAll() {
 		List<Category> list = repository.findAll();
-		
+
 		return list.stream().map(x -> new CategoryDto(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDto findById(Long id) {
+		Optional<Category> categoryOptional = repository.findById(id);
+		Category category = categoryOptional.get();
+		return new CategoryDto(category);
 	}
 }
