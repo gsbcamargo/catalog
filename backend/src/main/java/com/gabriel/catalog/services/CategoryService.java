d833d139646c4e700acb43d563a215f3de12353e
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2022-01-27 18:05:16 -0300

    fixed SQL seed issue and adjusted code styling

--- backend/src/main/java/com/gabriel/catalog/services/CategoryService.java
+++ backend/src/main/java/com/gabriel/catalog/services/CategoryService.java
@@ -4,18 +4,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gabriel.catalog.entities.Category;
 import com.gabriel.catalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
+
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
-
 }
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_category (name) VALUES ('Foo');
+INSERT INTO tb_category (name) VALUES ('Bar');
+INSERT INTO tb_category (name) VALUES ('Zar');
+INSERT INTO tb_category (name) VALUES ('Far');
