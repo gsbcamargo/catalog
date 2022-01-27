be0075c146d9eb8cd75c6826dd634445e69e2b1f
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2022-01-27 20:32:47 -0300

    created custom exception handler for controller

--- backend/src/main/java/com/gabriel/catalog/services/exceptions/EntityNotFoundException.java
+++ backend/src/main/java/com/gabriel/catalog/services/exceptions/EntityNotFoundException.java
@@ -1,9 +1,9 @@
 package com.gabriel.catalog.services.exceptions;
 
 public class EntityNotFoundException extends RuntimeException {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	public EntityNotFoundException(String message) {
 		super(message);
 	}
