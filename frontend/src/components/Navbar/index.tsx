feca9ade24d4b7cd7b7f4ca1a5621ae734b5e9fa
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2022-06-10 20:52:32 -0300

    implements navbar collapsable menu

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,26 +1,44 @@
 import './style.css';
+import 'bootstrap/js/src/collapse.js';
 
-function Navbar() {
+const Navbar = () => {
   return (
     <>
-      <nav className='navbar navbar-expand-md bg-secondary main-navbar'>
-        <div className='container-fluid'>
-          <a href='link' className='navbar-logo-title'>
+      <nav className="navbar navbar-expand-md navbar-dark bg-secondary main-navbar">
+        <div className="container-fluid">
+
+          <a href="link" className="navbar-logo-title">
             <h4>Catalog</h4>
           </a>
-          <div className='collapse navbar-collapse'>
-            <ul className='navbar-nav offset-md-2 main-menu'>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#dscatalog-navbar"
+            aria-controls="dscatalog-navbar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="dscatalog-navbar">
+            <ul className="navbar-nav offset-md-2 main-menu">
               <li>
-                <a href='link' className='active'>HOME</a>
+                <a href="link" className="active">
+                  HOME
+                </a>
               </li>
               <li>
-                <a href='link'>CATÁLOGO</a>
+                <a href="link">CATÁLOGO</a>
               </li>
               <li>
-                <a href='link'>ADMIN</a>
+                <a href="link">ADMIN</a>
               </li>
             </ul>
           </div>
+
         </div>
       </nav>
     </>
